878bcbdd128dfe863cd78bac386bdec720edf18a
LUKAS-R-DEV <rafaelbezerralucas@gmail.com>
2026-03-12 14:10:34 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  )
+}
